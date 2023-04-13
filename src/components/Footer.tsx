51147b38c08042e6ba2c6305daeb3b11d70b5b0c
mykhailokrachun <mykhailokrachun@gmail.com>
2023-04-13 16:49:03 +0300

    contact info fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,21 +7,27 @@ const Footer = () => {
   return (
     <footer className='relative h-auto bg-altGray w-screen flex flex-wrap justify-center items-center'>
       <div className='relative flex flex-wrap items-middle flex-col'>
-        <h1 className='text-whiteforform text-2xl text-center mt-6'>
+        <h2 className='text-whiteforform text-2xl text-center mt-6'>
           Mykhailo&apos;s Portfolio
-        </h1>
+        </h2>
         <p className='text-platina text-xl text-center px-4'>
           Thanks for visiting my personal portfolio website. Connect with me
           through contact form or socials
         </p>
-        <h1 className='text-whiteforform text-2xl text-center'>Contact info</h1>
-        <div className='flex flex-row justify-center items-center '>
-          <BsFillTelephoneFill className='text-xl text-platina mx-2 md:mx-3' />
-          <h1 className='text-platina text-xl md:mr-2'>+380 (50) 038 62 65</h1>
-          <GrMail className='text-xl text-platina mx-2 ' />
-          <h1 className='text-platina text-xl pr-4 md:pr-0'>
-            mykhailokrachun@gmail.com
-          </h1>
+        <h2 className='text-whiteforform text-2xl text-center'>Contact info</h2>
+        <div className='w-full flex flex-col md:flex-row justify-center items-center'>
+          <div className='flex items-center'>
+            <BsFillTelephoneFill className='text-xl text-platina mx-2 md:mx-3' />
+            <h3 className='text-platina text-xl md:mx-2'>
+              +380 (50) 038 62 65
+            </h3>
+          </div>
+          <div className='flex items-center'>
+            <GrMail className='text-xl text-platina mx-2 md:mx-3' />
+            <h3 className='text-platina text-xl md:mx-2'>
+              mykhailokrachun@gmail.com
+            </h3>
+          </div>
         </div>
         <div className='flex flex-row flex-wrap items-middle justify-center space-x-2 my-6'>
           <Link
